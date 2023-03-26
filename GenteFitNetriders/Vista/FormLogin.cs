ae01e7d08afafb1eaf1975e2ff81171f36be77a6
Antoni Moragues <amoraguesfo@uoc.edu>
2023-03-26 23:07:46 +0200

    Add Modelo y FormAdminPanel Test

--- GenteFitNetriders/Vista/FormLogin.cs
+++ GenteFitNetriders/Vista/FormLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenteFitNetriders.Vista;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,27 @@ namespace GenteFitNetriders
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string email = textUser.Text;
+            string pwd = textPassword.Text;
 
+            using (Modelo.NetridersEntities db = new Modelo.NetridersEntities())
+            {
+                var lst = from d in db.Usuarios
+                          where d.email == email
+                          && d.password == pwd
+                          select d;
+                if (lst.Count() > 0)
+                {
+                    this.Hide();
+                    FormAdminPanel formAdminPanel = new FormAdminPanel();
+                    formAdminPanel.FormClosed += (s, args) => this.Close();
+                    formAdminPanel.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Ususario o contraseña incorrectos");
+                }
+            }
         }
 
     }
