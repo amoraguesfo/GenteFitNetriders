9382d6be5f42d3023c952fc95f2547a56d9f0b38
Antoni Moragues <amoraguesfo@uoc.edu>
2023-03-26 22:33:55 +0200

    Add Vista folder

--- GenteFitNetriders/Vista/FormLogin.cs
+++ GenteFitNetriders/Vista/FormLogin.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace GenteFitNetriders
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
