1b5580eeb94b0b7f7311a2254ceef5498c406a4d
amoraguesfo <amoraguesfo@uoc.edu>
2023-03-30 16:11:40 +0200

    Add crud clase in Maincontroller

---
+++ GenteFitNetriders/Modelo/ReservaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenteFitNetriders.Modelo
+{
+    internal class ReservaViewModel
+    {
+        public int id { get; set; }
+        public int id_usuario { get; set; }
+        public int id_clase { get; set; }
+        public string estado { get; set; }
+    }
+}
